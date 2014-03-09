1f0f02f25dd4ec66431423df027f5e4725afd583
U10216007 <qaz13679@yahoo.com>
2014-03-09 17:27:36 +0800

    算出最後三個函數
    1.新增三個method算出三個函數：f(x)= cos(x) + 5sin(x)，f(x)= 5cos(x) + sin(x)，f(x)=
    log(x) + x^2
    2.在main method 中 print 出這三個函數的值

--- src/plot.java
+++ src/plot.java
@@ -13,6 +13,9 @@ public class plot {
 		System.out.println("g(x)= sin(x) : "  + gx(x));
 		System.out.println("h(x)= cos(x) : "  + hx(x));
 		System.out.println("j(x)= tan(x) : "  + jx(x));
+		System.out.println("k(x)= cos(x) + 5sin(x) : "  + kx(x));
+		System.out.println("n(x)= 5cos(x) + sin(x) : "  + nx(x));
+		System.out.println("m(x)= log(x) + x^2 : "  + mx(x));
 		
 	}
 		
@@ -32,5 +35,18 @@ public class plot {
 	public static double jx(int x){
 		return Math.tan(x);
 	}
+	
+	public static double kx(int x){
+		return Math.cos(x) + 5*Math.sin(x);
+	}
+	
+	public static double nx(int x){
+		return 5*Math.cos(x) + Math.sin(x);
+	}
+	
+	public static double mx(int x){
+		return Math.log(x) + x*x;
+	}
+	
 	
 }
